89a4b793c35ccb3f672eb3fd91a74b79eed2b2aa
The-Innominate <100097404+The-Innominate@users.noreply.github.com>
2024-03-26 08:37:08 -0600

    Initial Commit

---
+++ Assets/Scripts/PhysicsCollider.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsCollider : MonoBehaviour
+{
+    string status;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        status = "Collision Enter: " + collision.gameObject.name;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        status = "Trigger Enter: " + other.gameObject.name;
+    }
+    
+    private void OnTriggerStay(Collider other)
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+        GUI.skin.label.fontSize = 16;
+        Vector2 screen = Camera.main.WorldToScreenPoint(transform.position);
+        GUI.Label(new Rect(screen.x, Screen.height - screen.y, 250, 70), status);
+    }
+}
